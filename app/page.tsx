e968ecf9ee2e09dd312dbd7dcac1c1c5500319bd
midousdll <midousdll@gmail.com>
2026-08-15 15:10:41 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-1 items-center justify-center bg-secondary font-sans">
+      <main className="flex flex-col flex-1 w-full h-screen items-center justify-center py-32 px-16 bg-accent">
+        <h1 className="text-5xl font-bold text-primary">
+          Welcome to Next.js!
+        </h1>
+      </main>
+    </div>
+  );
+}
